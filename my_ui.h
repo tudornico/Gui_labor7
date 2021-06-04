1c20d0006c6151c2cb9511be9d0d257c8d7073db
tudornico <46244937+tudornico@users.noreply.github.com>
2021-06-04 18:28:38 +0300

    Update my_ui.h

--- my_ui.h
+++ my_ui.h
@@ -22,6 +22,12 @@ public:
 private slots:
     void on_Manager_button_clicked();
 
+    void on_customer_button_clicked();
+
+    void on_client_button_clicked();
+
+    void on_exit_button_clicked();
+
 private:
     Ui::My_UI *ui;
 };
