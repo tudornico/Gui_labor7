82cc5b8d19b2fda77da2d2de329384561359e944
tudornico <46244937+tudornico@users.noreply.github.com>
2021-06-04 18:28:19 +0300

    Update my_ui.cpp

--- my_ui.cpp
+++ my_ui.cpp
@@ -2,7 +2,9 @@
 #include "./ui_my_ui.h"
 #include <QPushButton>
 #include <QMainWindow>
-
+#include "Kunde.h"
+#include "Manager_ui.h"
+#include "customer_format.h"
 My_UI::My_UI(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::My_UI)
@@ -20,5 +22,33 @@ My_UI::~My_UI()
 void My_UI::on_Manager_button_clicked()
 {
 
+
+
+    this->hide();
+}
+
+
+void My_UI::on_customer_button_clicked()
+{
+    Customer_format c_format;
+    c_format.setModal(true);
+    c_format.exec();
+
+    this->hide();
+}
+
+
+void My_UI::on_client_button_clicked()
+{
+    QWidget *switch_widget=new QWidget;
+    switch_widget->show();
+    //ui->setupUi
+    this->hide();
+}
+
+
+void My_UI::on_exit_button_clicked()
+{
+   this->close();
 }
 
