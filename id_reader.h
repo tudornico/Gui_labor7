7d416809d024ec959ad0120d2238dd1fc54b871f
tudornico <46244937+tudornico@users.noreply.github.com>
2021-06-05 22:41:46 +0300

    Update id_reader.h

--- id_reader.h
+++ id_reader.h
@@ -2,7 +2,8 @@
 #define ID_READER_H
 
 #include <QDialog>
-
+#include "customer_format.h"
+#include "manager_format.h"
 namespace Ui {
 class id_reader;
 }
@@ -14,12 +15,14 @@ class id_reader : public QDialog
 public:
     explicit id_reader(QWidget *parent = nullptr);
     ~id_reader();
-
+    int get_id();
 private slots:
     void on_Submit_button_clicked();
 
 private:
     Ui::id_reader *ui;
+    Customer_format c_format;
+    Manager_format m_format;
 };
 
 #endif // ID_READER_H
