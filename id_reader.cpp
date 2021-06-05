10116d42f71848d88bd5f76eb15950e7fc6fba68
tudornico <46244937+tudornico@users.noreply.github.com>
2021-06-05 22:41:33 +0300

    Update id_reader.cpp

--- id_reader.cpp
+++ id_reader.cpp
@@ -1,13 +1,16 @@
 #include "id_reader.h"
 #include "ui_id_reader.h"
-
+int _id;
 id_reader::id_reader(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::id_reader)
 {
     ui->setupUi(this);
 }
-
+int id_reader::get_id()
+{
+    return _id;
+}
 id_reader::~id_reader()
 {
     delete ui;
@@ -15,6 +18,7 @@ id_reader::~id_reader()
 
 void id_reader::on_Submit_button_clicked()
 {
-
+    int id=ui->ID_line->text().toInt();
+    _id=id;
 }
 
