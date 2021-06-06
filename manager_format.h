ca56ad4e082325ed6e853f328669e7a1804941b2
tudornico <46244937+tudornico@users.noreply.github.com>
2021-06-06 11:54:33 +0300

    Update manager_format.h

--- manager_format.h
+++ manager_format.h
@@ -1,38 +1,110 @@
-#ifndef MANAGER_FORMAT_H
-#define MANAGER_FORMAT_H
-
-#include <QDialog>
-
-namespace Ui {
-class manager_format;
+#include "manager_format.h"
+#include "ui_manager_format.h"
+#include "reader.h"
+#include "Business.h"
+#include "id_reader.h"
+#include "carslist.h"
+#include <fstream>
+#include "Domain.h"
+#include <QTableWidgetItem>
+#include <QString>
+manager_format::manager_format(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::manager_format)
+{
+    ui->setupUi(this);
+    ifstream my_file("data_base.txt");
+    if(my_file.is_open())
+    {
+        string line;
+        while(getline(my_file,line))
+        {
+            stringstream ss(line);
+            const string model;
+            const string brand;
+            const string fuel;
+            const string year;
+            const string km;
+            const string price;
+            const string power;
+            getline(ss,model,',');
+            getline(ss,brand,',');
+            getline(ss,year,',');
+            getline(ss,km,',');
+            getline(ss,price,',');
+            getline(ss,power,',');
+            getline(ss,fuel,',');
+            Domain :: Car new_obj(model,brand,fuel,year,km,price,power);
+            
+        }
+    }
 }
 
-class manager_format : public QDialog
+manager_format::~manager_format()
 {
-    Q_OBJECT
+    delete ui;
+   
+}
+void manager_format::manager_show(){
 
-public:
-    explicit manager_format(QWidget *parent = nullptr);
-    ~manager_format();
-    void manager_show();
+    manager_format ui;
+}
 
-private slots:
-    void on_add_button_clicked();
 
-    void on_find_button_clicked();
 
-    void on_remove_button_clicked();
+void manager_format::on_find_button_clicked()
+{
+   id_reader new_id_reader;
+   new_id_reader.setModal(true);
+   new_id_reader.exec();
+   int id;
 
-    void on_add_button_2_clicked();
+}
 
-    void on_pushButton_clicked();
 
-    void on_exit_button_clicked();
+void manager_format::on_remove_button_clicked()
+{
+    id_reader new_id_reader;
+    new_id_reader.setModal(true);
+    new_id_reader.exec();
+    int id;
+}
 
-    void on_button_all_clicked();
 
-private:
-    Ui::manager_format *ui;
-};
 
-#endif // MANAGER_FORMAT_H
+void manager_format::on_exit_button_clicked()
+{
+    this->close();
+}
+
+
+void manager_format::on_button_all_clicked()
+{
+    CarsList new_list;
+    CarsList *switch_widget=new CarsList;
+    switch_widget->show();
+
+    this->hide();
+}
+
+
+void manager_format::on_button_add_clicked()
+{
+     reader new_reader;
+     new_reader.setModal(true);
+     new_reader.exec();
+     Domain::Car new_car=new_reader.get_car();
+     this->_rep.save(new_car);
+     std::ofstream my_file("data_base.txt");
+     if(my_file.is_open() == false)
+     {
+         this->destroy();
+     }
+
+     my_file<<endl;
+     my_file<<new_car.get_Model()<<","<<new_car.get_Brand()<<","<<new_car.get_Year()<<","
+    <<new_car.get_Kilometers()<<","<<new_car.get_Price()<<","<<new_car.get_Power()<<","<<new_car.get_Fuel();
+
+    my_file.close();
+}
+
